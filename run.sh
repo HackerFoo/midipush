aeabb5b4a994acc4a7aa5dd84dd04591086dc0cb
Dusty DeWeese <dustin.deweese@gmail.com>
2020-11-16 06:53:19 -0800

    automate aconnect in script

--- run.sh
+++ run.sh
@@ -2,8 +2,17 @@
 
 sudo modprobe snd_virmidi
 
+connect_midi() {
+    virtual=`aconnect -l | grep "Virtual Raw" -m 1 | sed -e 's/^client \([0-9]*\).*/\1/'`
+    synth=`aconnect -l | grep "Waldorf Kyra" -m 1 | sed -e 's/^client \([0-9]*\).*/\1/'`
+    if [ -n "$virtual" ] && [ -n "$synth" ]; then
+	aconnect ${virtual} ${synth}
+    fi
+}
+
 while true; do
     make
-    ./midipush
+    connect_midi
+    ./midipush 0 3
     sleep 2
 done
